762dd27925c010e8d0d4919d44d4e0d68ef9024d
tulili <tulilirockz.pub+git@gmail.com>
2023-09-28 01:06:38 -0300

    feat: general_configs for shell

--- .config/fish/conf.d/xdg-ninja.fish
+++ .config/fish/conf.d/xdg-ninja.fish
@@ -3,4 +3,5 @@ export XDG_CONFIG_HOME=$HOME/.config
 export XDG_STATE_HOME=$HOME/.local/state
 export XDG_CACHE_HOME=$HOME/.cache
 export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
-export GNUPGHOME="$XDG_DATA_HOME"/gnupg
+export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/podman/podman.sock
+export DOCKER_SOCK=$XDG_RUNTIME_DIR/podman/podman.sock
--- .profile
+++ .profile
@@ -3,5 +3,3 @@ export XDG_CONFIG_HOME=$HOME/.config
 export XDG_STATE_HOME=$HOME/.local/state 
 export XDG_CACHE_HOME=$HOME/.cache 
 export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
-export GNUPGHOME="$XDG_DATA_HOME"/gnupg
-
